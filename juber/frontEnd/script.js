c7b606f1f7b229b0afcb8d1ac9fa3bc71dfc6b1f
juber <juberk1010@gmail.com>
2023-12-05 11:46:47 +0530

    init

---
+++ juber/frontEnd/script.js
@@ -0,0 +1,45 @@
+const checkOutBtns = document.querySelectorAll('.btn')
+
+const checkOut = async(e) => {
+  try{
+    const amount = e.currentTarget.dataset.index;
+     const key = await fetch('http://localhost:4000/api/getkey');
+      const data = await fetch('http://localhost:4000/api/checkout' , {
+       method : "POST",
+       headers: {"Content-Type": "application/json"},
+       body : JSON.stringify({amount}),
+      })
+
+      const result = await data.json();
+      const dataKey = await key.json();
+      const {price , id} = result.order;
+
+      var options = {
+        key: dataKey.key,
+        amount: price, 
+        currency: "INR",
+        name: "Spark Foundation",
+        description: "Testing",
+        image: "https://avatars.githubusercontent.com/u/34296950?v=4",
+        order_id: id,
+        prefill: {
+            name: "John Doe",
+            email: "sparkFoundation@gmail.com",
+            contact: "34567894"
+        },
+        notes: {
+            "address": "Rohini sector 21 delhi"
+        },
+        theme: {
+            color: "#3399cc"
+        }
+    };
+      const razor = new Razorpay(options);
+      razor.open();
+    }catch(err){
+      console.log(err)
+    }
+}
+
+
+Array.from(checkOutBtns).forEach(btn => btn.addEventListener('click' , checkOut));
